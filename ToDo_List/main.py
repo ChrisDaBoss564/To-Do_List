8b6ce733e5a655fd7c9690f3b7fe85c5d01d42a0
ChrisDaBoss564 <161912504+ChrisDaBoss564@users.noreply.github.com>
2025-02-14 11:24:30 -0600

    Added Dictionary and functions

--- ToDo_List/main.py
+++ ToDo_List/main.py
@@ -1,1 +1,24 @@
 print("Hello World!")
+
+ToDo = {
+    "Monday": [],
+    "Tuesday": [],
+    "Wednesday": [],
+    "Thursday": [],
+    "Friday": [],
+    "Saturday": [],
+    "Sunday": []
+}
+
+def add_task(day, task):
+    ToDo[day].append(task);
+
+def remove_task(day, task):
+    ToDo[day].remove(task);
+
+def get_tasks(day):
+    return ToDo[day];
+
+def get_all_tasks():
+    return ToDo;
+
